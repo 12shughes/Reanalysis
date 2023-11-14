3e6d78494cdbc6f5b97d9e29b2ea6ad230edb64e
12shughes <sh1293@exeter.ac.uk>
2023-11-14 11:42:47 +0000

    do the regridding in the python file

--- save_grid.py
+++ save_grid.py
@@ -2,7 +2,7 @@ import xarray as xr
 import glob
 import os
 ompath = '/disco/share/sh1293/OpenMars_data/openmars_my28_ls109_my28_ls124.nc'
-empath = 'disco/share/sh1293/EMARS_data/'
+empath = '/disco/share/sh1293/EMARS_data/'
 ds = xr.open_dataset(ompath)
 lats = ds['lat'].values.flatten()
 lons = ds['lon'].values.flatten()
@@ -15,6 +15,6 @@ os.chdir(empath + 'Raw/')
 infiles = glob.glob('emars*.nc')
 os.chdir(home)
 for infile in infiles:
-    ds = xr.open_dataset(infile)
+    ds = xr.open_dataset(empath + 'Raw/' + infile)
     outfile = ds.interp(lat=lats, lon=lons)
     outfile.to_netcdf(empath + 'Regrid/' + infile)
