034e479fb3fa246a9ba533e64ad35cc220940ea2
12shughes <sh1293@exeter.ac.uk>
2023-11-14 11:38:03 +0000

    edit save_grid to do xarray interp

--- save_grid.py
+++ save_grid.py
@@ -1,9 +1,20 @@
 import xarray as xr
-path = '/disco/share/sh1293/OpenMars_data/openmars_my28_ls109_my28_ls124.nc'
-ds = xr.open_dataset(path)
+import glob
+import os
+ompath = '/disco/share/sh1293/OpenMars_data/openmars_my28_ls109_my28_ls124.nc'
+empath = 'disco/share/sh1293/EMARS_data/'
+ds = xr.open_dataset(ompath)
 lats = ds['lat'].values.flatten()
 lons = ds['lon'].values.flatten()
-with open('/disco/share/sh1293/OpenMars_data/gridfile.txt', 'w') as f:
-    f.write('# lon lat\n')
-    for lon, lat in zip(lons, lats):
-        f.write(f'{lon} {lat}\n')
+#with open('/disco/share/sh1293/OpenMars_data/gridfile.txt', 'w') as f:
+#    f.write('# lon lat\n')
+#    for lon, lat in zip(lons, lats):
+#        f.write(f'{lon} {lat}\n')
+home = os.getenv("HOME")
+os.chdir(empath + 'Raw/')
+infiles = glob.glob('emars*.nc')
+os.chdir(home)
+for infile in infiles:
+    ds = xr.open_dataset(infile)
+    outfile = ds.interp(lat=lats, lon=lons)
+    outfile.to_netcdf(empath + 'Regrid/' + infile)
