3f8c7a68a2a8606145f1091704eb8bdfdae65f64
12shughes <sh1293@exeter.ac.uk>
2023-11-20 10:08:50 +0000

    correct for using multiple files

--- save_grid.py
+++ save_grid.py
@@ -3,9 +3,9 @@ import glob
 import os
 ompath = '/disco/share/sh1293/OpenMars_data/openmars_my28_ls109_my28_ls124.nc'
 empath = '/disco/share/sh1293/EMARS_data/'
-ds = xr.open_dataset(ompath)
-lats = ds['lat'].values.flatten()
-lons = ds['lon'].values.flatten()
+d = xr.open_dataset(ompath)
+lats = d['lat'].values.flatten()
+lons = d['lon'].values.flatten()
 #with open('/disco/share/sh1293/OpenMars_data/gridfile.txt', 'w') as f:
 #    f.write('# lon lat\n')
 #    for lon, lat in zip(lons, lats):
@@ -16,5 +16,9 @@ infiles = glob.glob('emars*.nc')
 os.chdir(home)
 for infile in infiles:
     ds = xr.open_dataset(empath + 'Raw/' + infile)
-    outfile = ds.interp(lat=lats, lon=lons)
+    ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180)).sortby('lon')
+    ds = ds.assign_coords(lonv=(((ds.lonv + 180) % 360) - 180)).sortby('lonv')
+    outfile = ds.interp(lon=lons, lonv = lons, wrap=True)
+    outfile = outfile.interp(lat=lats, latu=lats)
+    #outfile = outfile.transpose('time', 'lat', 'lon', 'ilev', 'latu', 'lonv')
     outfile.to_netcdf(empath + 'Regrid/' + infile)
