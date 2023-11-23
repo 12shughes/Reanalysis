b696a1cf9e283f4d75d0cccede9701dcdf5bf10f
12shughes <sh1293@exeter.ac.uk>
2023-11-23 15:39:42 +0000

    change file to open all files at once

--- processing.py
+++ processing.py
@@ -7,16 +7,13 @@ import glob
 import xarray as xr
 
 opath = '/disco/share/sh1293/OpenMARS_data/Raw/'
-
-files = glob.glob(opath)
-for file in files:
-    print(file)
-    initds = xr.open_mfdataset(file)
-    print('prepping ds')
-    midds, prs = calc.netcdf_prep(initds)
-    print('interpolating to isobaric')
-    d_isobaric = calc.isobaric_interp(midds, prs)
-    theta, d_isobaric['PV'] = calc.calculate_PV(d_isobaric)
-    print('interpolating to isentropic')
-    d_isentropic = calc.interpolate_to_isentropic(d_isobaric)
-    d_isentropic.to_netcdf('/disco/share/sh1293/OpenMARS_data/' + file + '.nc')
+print('opening data')
+initds = xr.open_mfdataset(opath + '*.nc')
+print('prepping ds')
+midds, prs = calc.netcdf_prep(initds)
+print('interpolating to isobaric')
+d_isobaric = calc.isobaric_interp(midds, prs)
+theta, d_isobaric['PV'] = calc.calculate_PV(d_isobaric)
+print('interpolating to isentropic')
+d_isentropic = calc.interpolate_to_isentropic(d_isobaric)
+d_isentropic.to_netcdf('/disco/share/sh1293/OpenMARS_data/' + file + '.nc')
