115ce563b4af289460d8417518f1a42561a1b37c
12shughes <sh1293@exeter.ac.uk>
2023-11-23 15:19:22 +0000

    create processing script

--- download_openmars.sh
+++ download_openmars.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rsync --dry-run https://ordo.open.ac.uk/ndownloader/articles/24573205/versions/1 /disco/share/sh1293/OpenMARS_data/Raw
+wget -P  /disco/share/sh1293/OpenMARS_data/Raw https://ordo.open.ac.uk/ndownloader/articles/24573205/versions/1
