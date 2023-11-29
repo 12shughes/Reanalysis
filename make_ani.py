e67a4e69916e88f11c41877db894abd4c4d27d78
12shughes <sh1293@exeter.ac.uk>
2023-11-29 10:43:56 +0000

    code to stitch together pngs into animation

---
+++ make_ani.py
@@ -0,0 +1,3 @@
+import os
+os.system('convert -delay 3 /disco/share/sh1293/OpenMARS_data/Isentropic/Plots/ctf_my28*.png /home/links/sh1293/Reanalysis/Animations/ctf_MY28.gif')
+#os.system('ffmpeg -f gif -i /home/links/sh1293/Reanalysis/Animations/ctf_MY28.gif home/links/sh1293/Reanalysis/Animations/ctf_MY28.mp4')
