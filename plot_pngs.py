068fe3f8b31562487d630db4ca8b498f9279b9d1
12shughes <sh1293@exeter.ac.uk>
2023-11-30 15:58:28 +0000

    run for all years

--- plot_pngs.py
+++ plot_pngs.py
@@ -16,61 +16,66 @@ import math
 from matplotlib import (cm, colors)
 
 path = '/disco/share/sh1293/OpenMARS_data/Isentropic/'
-my = 28
-files = glob.glob(path + '*my%.0f.nc' %(my))
-list = range(361)
-for file in files:
-    ds = xr.open_dataset(file)
-    ds['PV'] = ds['PV'] * (ds['level'] / 200)**(-(1+1/0.25))
-    d = ds.PV[:,4,:9,:]*10**4
-    d['Ls'] = ds.Ls[:,0].drop_vars('lon')
-    d = d.set_index(time='Ls')
+
+years = [27, 29, 30, 31, 32, 33, 34, 35, 36]
+
+#my = 28
+
+for my in years:
+    files = glob.glob(path + '*my%.0f.nc' %(my))
+    list = range(361)
+    for file in files:
+        ds = xr.open_dataset(file)
+        ds['PV'] = ds['PV'] * (ds['level'] / 200)**(-(1+1/0.25))
+        d = ds.PV[:,4,:9,:]*10**4
+        d['Ls'] = ds.Ls[:,0].drop_vars('lon')
+        d = d.set_index(time='Ls')
 
 
-fig, ax = plt.subplots(figsize = (10,10), subplot_kw={'projection':ccrs.NorthPolarStereo()})
+    fig, ax = plt.subplots(figsize = (10,10), subplot_kw={'projection':ccrs.NorthPolarStereo()})
 
-theta = np.linspace(0, 2*np.pi, 100)
-center, radius = [0.5, 0.5], 0.5
-verts = np.vstack([np.sin(theta), np.cos(theta)]).T
-circle = mpath.Path(verts * radius + center)
-gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = 1, linestyle = '-', color = 'black', alpha = 1, draw_labels=True)
-ax.set_boundary(circle, transform=ax.transAxes)
-ax.set_extent([-180,180,50,90], crs=ccrs.PlateCarree())
-
-
-
-#vmin = 1
-#vmax = 6.6
-#step = 0.5
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
+    gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = 1, linestyle = '-', color = 'black', alpha = 1, draw_labels=True)
+    ax.set_boundary(circle, transform=ax.transAxes)
+    ax.set_extent([-180,180,50,90], crs=ccrs.PlateCarree())
 
 
-# contourf or pcolormesh
-type = 'contourf'
 
-if type == 'contourf':
-    for i in range(len(d[:,0,0].values)):
+    #vmin = 1
+    #vmax = 6.6
+    #step = 0.5
+
+
+    # contourf or pcolormesh
+    type = 'contourf'
+
+    if type == 'contourf':
+        for i in range(len(d[:,0,0].values)):
+            fig, ax = plt.subplots(figsize = (10,10), subplot_kw={'projection':ccrs.NorthPolarStereo()})
+            gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = 1, linestyle = '-', color = 'black', alpha = 1, draw_labels=True)
+            ax.set_boundary(circle, transform=ax.transAxes)
+            ax.set_extent([-180,180,50,90], crs=ccrs.PlateCarree())
+            contourplot = ax.contourf(d[i,:,:].lon, d[i,:,:].lat, d[i,:,:].values, vmin = 0, vmax = 8,
+                                        transform = ccrs.PlateCarree(), cmap='viridis', levels=np.linspace(0, 8, 21), extend = 'both')
+            cbar = plt.colorbar(contourplot, ticks = np.linspace(0,8,11), shrink = 0.5, fraction = 0.075)
+            plt.title('MY%02d Ls%.4f' %(my, d.time[i].values))
+            fig.tight_layout()
+            plt.savefig(path + '/Plots/MY%02d/ctf_my%02dLs%03d_%04d.png' %(my, my, math.modf(d.time[i].values)[1], (math.modf(d.time[i].values)[0])*10**4))
+    elif type == 'pcolormesh':
+        i = 0
+        #for i in range(len(d[:,0,0].values)):
         fig, ax = plt.subplots(figsize = (10,10), subplot_kw={'projection':ccrs.NorthPolarStereo()})
         gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = 1, linestyle = '-', color = 'black', alpha = 1, draw_labels=True)
         ax.set_boundary(circle, transform=ax.transAxes)
         ax.set_extent([-180,180,50,90], crs=ccrs.PlateCarree())
-        contourplot = ax.contourf(d[i,:,:].lon, d[i,:,:].lat, d[i,:,:].values, vmin = 0, vmax = 8,
-                                    transform = ccrs.PlateCarree(), cmap='viridis', levels=np.linspace(0, 8, 21), extend = 'both')
-        cbar = plt.colorbar(contourplot, ticks = np.linspace(0,8,11), shrink = 0.5, fraction = 0.075)
+        X, Y = np.meshgrid(d[i,:,:].lon, d[i,:,:].lat)
+        contourplot = ax.pcolormesh(X, Y, d[i,:,:].values, transform = ccrs.PlateCarree())# vmin = 0, vmax = 8,
+                                    #transform = ccrs.PlateCarree(), cmap='viridis')
+        cbar = plt.colorbar(contourplot, ticks = np.linspace(0,8,11), shrink = 0.5, fraction = 0.075, extend = 'both')
         plt.title('MY%02d Ls%.4f' %(my, d.time[i].values))
         fig.tight_layout()
-        plt.savefig(path + '/Plots/MY%02d/ctf_my%02dLs%03d_%04d.png' %(my, my, math.modf(d.time[i].values)[1], (math.modf(d.time[i].values)[0])*10**4))
-elif type == 'pcolormesh':
-    i = 0
-    #for i in range(len(d[:,0,0].values)):
-    fig, ax = plt.subplots(figsize = (10,10), subplot_kw={'projection':ccrs.NorthPolarStereo()})
-    gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = 1, linestyle = '-', color = 'black', alpha = 1, draw_labels=True)
-    ax.set_boundary(circle, transform=ax.transAxes)
-    ax.set_extent([-180,180,50,90], crs=ccrs.PlateCarree())
-    X, Y = np.meshgrid(d[i,:,:].lon, d[i,:,:].lat)
-    contourplot = ax.pcolormesh(X, Y, d[i,:,:].values, transform = ccrs.PlateCarree())# vmin = 0, vmax = 8,
-                                #transform = ccrs.PlateCarree(), cmap='viridis')
-    cbar = plt.colorbar(contourplot, ticks = np.linspace(0,8,11), shrink = 0.5, fraction = 0.075, extend = 'both')
-    plt.title('MY%02d Ls%.4f' %(my, d.time[i].values))
-    fig.tight_layout()
-    plt.savefig(path + '/Plots/MY%02d/pcm_my%02dLs%03d_%04d.png' %(my, my, math.modf(d.time[i].values)[1], (math.modf(d.time[i].values)[0])*10**4))
+        plt.savefig(path + '/Plots/MY%02d/pcm_my%02dLs%03d_%04d.png' %(my, my, math.modf(d.time[i].values)[1], (math.modf(d.time[i].values)[0])*10**4))
 
