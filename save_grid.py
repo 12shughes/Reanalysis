db7c93b1caf11402d6c69c69c975bcd1bd422978
12shughes <sh1293@exeter.ac.uk>
2023-12-13 14:44:37 +0000

    update the paths for the new OpenMARS directory and the splitting of EMARS into control and analysis

--- save_grid.py
+++ save_grid.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import glob
 import os
-ompath = '/disco/share/sh1293/OpenMars_data/openmars_my28_ls109_my28_ls124.nc'
+ompath = '/disco/share/sh1293/OpenMARS_data/Raw/openmars_my28_ls109_my28_ls124.nc'
 empath = '/disco/share/sh1293/EMARS_data/'
 d = xr.open_dataset(ompath)
 lats = d['lat'].values.flatten()
@@ -11,14 +11,15 @@ lons = d['lon'].values.flatten()
 #    for lon, lat in zip(lons, lats):
 #        f.write(f'{lon} {lat}\n')
 home = os.getenv("HOME")
-os.chdir(empath + 'Raw/')
-infiles = glob.glob('emars*.nc')
-os.chdir(home)
-for infile in infiles:
-    ds = xr.open_dataset(empath + 'Raw/' + infile)
-    ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180)).sortby('lon')
-    ds = ds.assign_coords(lonv=(((ds.lonv + 180) % 360) - 180)).sortby('lonv')
-    outfile = ds.interp(lon=lons, lonv = lons, wrap=True)
-    outfile = outfile.interp(lat=lats, latu=lats)
-    #outfile = outfile.transpose('time', 'lat', 'lon', 'ilev', 'latu', 'lonv')
-    outfile.to_netcdf(empath + 'Regrid/' + infile)
+for type in ['Control/']:
+    os.chdir(empath + type + 'Raw/')
+    infiles = glob.glob('emars*.nc')
+    os.chdir(home)
+    for infile in infiles:
+        ds = xr.open_dataset(empath + type + 'Raw/' + infile)
+        ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180)).sortby('lon')
+        ds = ds.assign_coords(lonv=(((ds.lonv + 180) % 360) - 180)).sortby('lonv')
+        outfile = ds.interp(lon=lons, lonv = lons, wrap=True)
+        outfile = outfile.interp(lat=lats, latu=lats)
+        #outfile = outfile.transpose('time', 'lat', 'lon', 'ilev', 'latu', 'lonv')
+        outfile.to_netcdf(empath + type + 'Regrid/' + infile)
