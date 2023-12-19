4510de9764b2a54cc415f61dd98961c8c2882667
12shughes <sh1293@exeter.ac.uk>
2023-12-19 14:48:13 +0000

    add scaled_eddy_enstrophy function

--- functions.py
+++ functions.py
@@ -33,3 +33,25 @@ def eddy_enstrophy(q, **kwargs):
     Z = qp.sum(dim = 'lat').sum(dim = 'lon') / (cos.sum(dim = 'lat') * 2 * np.pi)
     
     return Z
+
+
+def scaled_eddy_enstrophy(q, **kwargs):
+    '''
+    Calculate the eddy enstrophy from dataarray q
+    '''
+    latmin = kwargs.pop('latmin', 60)
+
+    q = q.where(q.lat >= latmin, drop = True)
+    q = q.where(q.lon < 179.5, drop = True)
+    qbar = q.mean(dim = 'lon')
+    qbar = qbar.expand_dims({'lon':q.lon})
+
+    qprime = (q - qbar) / qbar
+
+    cos = np.cos(np.deg2rad(q.lat))
+
+    qp = qprime **2 * cos
+
+    Z = qp.sum(dim = 'lat').sum(dim = 'lon') / (cos.sum(dim = 'lat') * 2 * np.pi)
+    
+    return Z
