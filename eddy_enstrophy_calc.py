eb576fb149cb3f1ea95d91549549a61d3283398e
12shughes <sh1293@exeter.ac.uk>
2023-12-19 14:57:39 +0000

    add scaled calculation and path existence criterion

--- eddy_enstrophy_calc.py
+++ eddy_enstrophy_calc.py
@@ -1,6 +1,7 @@
 import xarray as xr
 import numpy as np
 import functions as fcs
+import os
 
 islev = 000
 # dataset can be OpenMARS_data, EMARS_data/Control, EMARS_data/Analysis
@@ -27,19 +28,27 @@ path = '/disco/share/sh1293/%s/Isentropic/' %(dataset)
 
 for year in years:
     print(year)
-    print('Opening dataset')
-    ds = xr.open_dataset(path + 'isentropic_%s_my%02d.nc' %(set, year))
-    print('Organising data')
-    if islev != 000:
-        ds = ds.where(ds.level == islev, drop = True)
-        ds['Ls'] = ds.Ls[:,0,0].drop_vars('lon').drop_vars('level')
-    elif islev == 000:
-        ds['Ls'] = ds.Ls[:,0].drop_vars('lon')
-    da = ds.PV * 10**4
-    da = da.assign_coords({'Ls':ds.Ls})
-    print('Lait scaling')
-    qs = fcs.lait_scale(da)
-    print('Eddy enstrophy calculation')
-    edd_ens = fcs.eddy_enstrophy(qs)
-    print('Saving')
-    edd_ens.to_netcdf('/disco/share/sh1293/%s/Eddy_enstrophy/lev%03d_my%02d.nc' %(dataset, islev, year))
+    if not os.path.exists('/disco/share/sh1293/%s/Eddy_enstrophy/lev%03d_my%02d.nc' %(dataset, islev, year))\
+        or not os.path.exists('/disco/share/sh1293/%s/Eddy_enstrophy/scaled_lev%03d_my%02d.nc' %(dataset, islev, year)):
+        print('Opening dataset')
+        ds = xr.open_dataset(path + 'isentropic_%s_my%02d.nc' %(set, year))
+        print('Organising data')
+        if islev != 000:
+            ds = ds.where(ds.level == islev, drop = True)
+            ds['Ls'] = ds.Ls[:,0,0].drop_vars('lon').drop_vars('level')
+        elif islev == 000:
+            ds['Ls'] = ds.Ls[:,0].drop_vars('lon')
+        da = ds.PV * 10**4
+        da = da.assign_coords({'Ls':ds.Ls})
+        print('Lait scaling')
+        qs = fcs.lait_scale(da)
+        if not os.path.exists('/disco/share/sh1293/%s/Eddy_enstrophy/lev%03d_my%02d.nc' %(dataset, islev, year)):
+            print('Eddy enstrophy calculation')
+            edd_ens = fcs.eddy_enstrophy(qs)
+            print('Saving')
+            edd_ens.to_netcdf('/disco/share/sh1293/%s/Eddy_enstrophy/lev%03d_my%02d.nc' %(dataset, islev, year))
+        if not os.path.exists('/disco/share/sh1293/%s/Eddy_enstrophy/scaled_lev%03d_my%02d.nc' %(dataset, islev, year)):
+            print('Scaled eddy enstrophy calculation')
+            sc_edd_ens = fcs.scaled_eddy_enstrophy(qs)
+            print('Saving')
+            sc_edd_ens.to_netcdf('/disco/share/sh1293/%s/Eddy_enstrophy/scaled_lev%03d_my%02d.nc' %(dataset, islev, year))
