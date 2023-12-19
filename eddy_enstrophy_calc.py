86af6845b5e06cb0ffda2e038e16bcd0a07b370d
12shughes <sh1293@exeter.ac.uk>
2023-12-19 14:39:18 +0000

    update so it can be run for OpenMARS or the two EMARS data types by a simple user input

--- eddy_enstrophy_calc.py
+++ eddy_enstrophy_calc.py
@@ -3,14 +3,29 @@ import numpy as np
 import functions as fcs
 
 islev = 000
+# dataset can be OpenMARS_data, EMARS_data/Control, EMARS_data/Analysis
+dataset = raw_input('Enter directory name (from OpenMARS_data, EMARS_data/Control, EMARS_data/Analysis): ')
+for dataset not in ['OpenMARS_data', 'EMARS_data/Control', 'EMARS_data/Analysis']:
+    print('Incorrect input')
+    dataset = raw_input('Enter directory name (from OpenMARS_data, EMARS_data/Control, EMARS_data/Analysis): ')
+
+if dataset == 'OpenMARS_data':
+    set = 'openmars'
+    years = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+elif dataset == 'EMARS_data/Control':
+    set = 'emars'
+    years = [24, 25, 26, 27]
+elif dataset == 'EMARS_data/Analysis':
+    set = 'emars'
+    years = [24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
 
 
-path = '/disco/share/sh1293/OpenMARS_data/Isentropic/'
-years = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+path = '/disco/share/sh1293/%s/Isentropic/' %(dataset)
+
 for year in years:
     print(year)
     print('Opening dataset')
-    ds = xr.open_dataset(path + 'isentropic_openmars_my%02d.nc' %(year))
+    ds = xr.open_dataset(path + 'isentropic_%s_my%02d.nc' %(set, year))
     print('Organising data')
     if islev != 000:
         ds = ds.where(ds.level == islev, drop = True)
@@ -24,4 +39,4 @@ for year in years:
     print('Eddy enstrophy calculation')
     edd_ens = fcs.eddy_enstrophy(qs)
     print('Saving')
-    edd_ens.to_netcdf('/disco/share/sh1293/OpenMARS_data/Eddy_enstrophy/lev%03d_my%02d.nc' %(islev, year))
+    edd_ens.to_netcdf('/disco/share/sh1293/%s/Eddy_enstrophy/lev%03d_my%02d.nc' %(dataset, islev, year))
